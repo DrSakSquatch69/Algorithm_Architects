3d33046ad40fb41ae38019b70ccab8b6bb76d734
Anthony Balladares <aballadares647@gmail.com>
2024-10-13 15:22:38 -0400

    Added a gun model

--- Algorithm Architects/Assets/Scripts/gameManager.cs
+++ Algorithm Architects/Assets/Scripts/gameManager.cs
@@ -10,7 +10,7 @@ public class gameManager : MonoBehaviour
     public static gameManager instance;                                     //how we will access game manager
                                                                             // Start is called before the first frame update
 
-    [SerializeField] GameObject menuActive, menuPause, menuWin, menuLose, menuNextLevel, hitMarker, screenFlash, reloading, noAmmo, menuTutorialPause;
+    [SerializeField] GameObject menuActive, menuPause, menuWin, menuLose, menuNextLevel, hitMarker, screenFlash, reloading, noAmmo, menuTutorialPause, muzzleFlash;
     //[SerializeField] GameObject dialogueBox;                                //Set apart so it can be commented out / turned off
     [SerializeField] TMP_Text enemyCountText;
     [SerializeField] TMP_Text remainingAmmoText;
