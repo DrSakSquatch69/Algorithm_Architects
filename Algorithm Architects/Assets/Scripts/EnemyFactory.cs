371db03bee9502525e4ba6690be1292f42113aae
Jeffery Brewer <jcbrewer1@student.fullsail.edu>
2024-10-28 19:34:29 -0500

    test

--- Algorithm Architects/Assets/Scripts/EnemyFactory.cs
+++ Algorithm Architects/Assets/Scripts/EnemyFactory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//
 public class EnemyFactory : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
