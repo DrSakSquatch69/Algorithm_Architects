d6030ebc810d4a0663eab2257d26c4e8e3b6eb2d
Chelsea Adams <chelsea.adams999@gmail.com>
2024-10-31 02:08:13 -0400

    Burrowing Carrot Prefab

--- Algorithm Architects/Assets/Scripts/CarrotAI.cs
+++ Algorithm Architects/Assets/Scripts/CarrotAI.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 //
-public class EnemyAI : MonoBehaviour, IDamage
+public class CarrotAI : MonoBehaviour, IDamage
 {
     enum damageTypes { bullet, chaser, stationary, butter }
 
@@ -20,6 +20,7 @@ public class EnemyAI : MonoBehaviour, IDamage
     [SerializeField] GameObject bullet;
     [SerializeField] float firerate;
     [SerializeField] int rotateSpeed;
+    [SerializeField] float burrowedPos;
 
     int hpOrig;                                 //Original HP
     [SerializeField] int HP;
@@ -38,14 +39,16 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     bool isShooting;
     bool playerSighted;
+    bool inGround;
 
+    float origPos;
     int currentRespawnCount = 1;
     //int activeEnemiesAI; //Used for tracking the active enemies 
 
     public Image enemyHp;
     Image enemyHpBar;
     public bool isImgOn;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -54,6 +57,7 @@ public class EnemyAI : MonoBehaviour, IDamage
         colorOrig = model.material.color;
         hpOrig = HP;                                //set original hp
         render = GetComponent<Renderer>();        //getting the renderer of the game object
+        origPos = transform.position.y;
         enemyHpBar = Instantiate(enemyHp, FindObjectOfType<Canvas>().transform).GetComponent<Image>();
         enemyHpBar.transform.SetParent(gameManager.instance.enemyHpParent.transform);
         gameManager.instance.updateGameGoal(1);
@@ -66,22 +70,27 @@ public class EnemyAI : MonoBehaviour, IDamage
     void Update()
     {
         updateEnemyUI();
-       // activeEnemiesAI = GameObject.FindGameObjectsWithTag("Enemy").Length; //Checks for the current amount of remaining active enemies
+        // activeEnemiesAI = GameObject.FindGameObjectsWithTag("Enemy").Length; //Checks for the current amount of remaining active enemies
         agent.SetDestination(gameManager.instance.getPlayer().transform.position);
         
-        if(playerSighted && canSeePlayer())
+        if (playerSighted && canSeePlayer())
         {
-            
+        }
+        else
+        {
+            inGround = true;
+            burrow();
         }
     }
     bool canSeePlayer()
     {
+        inGround = false;
         playerDirection = gameManager.instance.getPlayer().transform.position - headPosition.position;
         angleToPlayer = Vector3.Angle(playerDirection, transform.forward);
         Debug.DrawRay(headPosition.position, playerDirection);
 
         RaycastHit hit;
-        if (Physics.Raycast(headPosition.position, playerDirection, out hit, 1000 ,~ignoreMask))
+        if (Physics.Raycast(headPosition.position, playerDirection, out hit, 1000, ~ignoreMask))
         {
             if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
             {
@@ -110,48 +119,49 @@ public class EnemyAI : MonoBehaviour, IDamage
         //when hp is zero or less, it destroys the object
         if (HP <= 0)
         {
-           // --activeEnemiesAI;
-           // gameManager.instance.ActiveCheck(activeEnemiesAI);
+            // --activeEnemiesAI;
+            // gameManager.instance.ActiveCheck(activeEnemiesAI);
 
             // Check if enemy can respawn
             if (currentRespawnCount < maxRespawns)
             {
-                
+
 
                 //Creates two new enemies when this one dies
                 GameObject enemy1 = Instantiate(enemyPrefab, transform.position + Vector3.right, Quaternion.identity); // offset position so theyre not stacked
                 GameObject enemy2 = Instantiate(enemyPrefab, transform.position + Vector3.left, Quaternion.identity); // offset position so theyre not stacked
-                
+
                 // Set the respawn count of the new enemies to be 1 more than the current enemy
                 enemy1.GetComponent<EnemyAI>().SetRespawnCount(currentRespawnCount + 1);
                 enemy2.GetComponent<EnemyAI>().SetRespawnCount(currentRespawnCount + 1);
-                
+
                 //Increment the game goal by 1 for each new enemy
                 gameManager.instance.updateGameGoal(+1);
-                
+
             }
-            else 
+            else
             {
                 // No more respawns allowed, decrement the game goal
                 gameManager.instance.updateGameGoal(-1);
-               
+
             }
 
             // Destroys current enemy
             Destroy(gameObject);
-            
+
             //if (gameManager.instance.ActiveCheck(activeEnemiesAI))
             //{
             //    gameManager.instance.Waves();
             //}
         }
     }
-   
+
     public void updateEnemyUI()
     {
         float dist = Vector3.Distance(transform.position, gameManager.instance.getPlayer().transform.position);  //get the distance between the player and enemy
 
-        if (render.isVisible && dist <= renderDistance) {                                                                                             //see if enemy model is on screen
+        if (render.isVisible && dist <= renderDistance)
+        {                                                                                             //see if enemy model is on screen
             enemyHpBar.enabled = true;
             isImgOn = true;
             enemyHpBar.fillAmount = (float)HP / hpOrig;                                                                     //update enemy hp bar fill amount
@@ -159,7 +169,8 @@ public class EnemyAI : MonoBehaviour, IDamage
             dist = 1 / dist * 10f;
             dist = Mathf.Clamp(dist, minHPSize, maxHPSize);                                                                            //set min and max for what dist can be
             enemyHpBar.transform.localScale = new Vector3(dist, dist, 0);                                        //set scale based on distance
-        } else
+        }
+        else
         {
             enemyHpBar.enabled = false;                                                                         //turn off health bar if enemy is not on screen
             isImgOn = false;
@@ -209,7 +220,15 @@ public class EnemyAI : MonoBehaviour, IDamage
         isShooting = false;
     }
 
-  
+    void burrow()
+    {
+        if (inGround)
+        {
+            transform.position = new Vector3(transform.position.x, burrowedPos, transform.position.z);
+        }
+        else if (!inGround)
+        {
+            transform.position = new Vector3(transform.position.x, origPos, transform.position.z);
+        }
+    }
 }
-
-
